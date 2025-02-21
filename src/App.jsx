dfe11bace687fc9aa05f59db210c28aa337b3c4e
melok2207 <132545373+melok2207@users.noreply.github.com>
2025-02-21 23:21:44 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
