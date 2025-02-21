933bce3b4170a91ed15ea9894bbe00b717cb6184
melok2207 <132545373+melok2207@users.noreply.github.com>
2025-02-21 23:19:43 +0200

    Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 React Project. Всі права захищені.</p>
+    </footer>
+  );
+}
+
+export default Footer;
